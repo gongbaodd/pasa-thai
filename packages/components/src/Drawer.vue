a65158d19ed6c5b7504a5fd8bf78af5122df5152
gong@UbuntuWSLProArt <gongaodd@gmail.com>
2024-02-18 19:22:16 +0700

    Refactor app.vue and package.json, add Drawer component

---
+++ packages/components/src/Drawer.vue
@@ -0,0 +1,23 @@
+<template>
+    <ion-app>
+      <ion-split-pane content-id="main-content">
+        <ion-menu content-id="main-content" type="overlay">
+          <ion-content>
+            <ion-list id="inbox-list">
+              <ion-list-header>Inbox</ion-list-header>
+              <ion-note>hi@ionicframework.com</ion-note>
+  
+              <!-- <ion-menu-toggle :auto-hide="false" v-for="(p, i) in appPages" :key="i">
+                <ion-item @click="selectedIndex = i" router-direction="root" :router-link="p.url" lines="none" :detail="false" class="hydrated" :class="{ selected: selectedIndex === i }">
+                  <ion-icon aria-hidden="true" slot="start" :ios="p.iosIcon" :md="p.mdIcon"></ion-icon>
+                  <ion-label>{{ p.title }}</ion-label>
+                </ion-item>
+              </ion-menu-toggle> -->
+            </ion-list>
+  
+          </ion-content>
+        </ion-menu>
+        <slot />
+      </ion-split-pane>
+    </ion-app>
+  </template>
