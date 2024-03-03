1cea93a9ff03a5254f751649f97ccffd2b3d9d88
gong@UbuntuWSLProArt <gongaodd@gmail.com>
2024-03-03 18:48:43 +0700

    fix nuxt error

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ import { defineNuxtConfig } from "nuxt/config";
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/ionic"],
+  ssr: false,
   routeRules: {
     "/": {
       prerender: false,
