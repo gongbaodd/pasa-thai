5bb0e5ccb1bbdad0608facd68fa2f4564650e6f2
gongbaodd@ProArt <gongbaodd@qq.com>
2024-03-07 07:40:16 +0700

    fix filter words

--- packages/vmodels/daily.ts
+++ packages/vmodels/daily.ts
@@ -6,7 +6,9 @@ import dayjs from "dayjs"
 const filteredWords = computed(() => {
     const ws = words.filter(word => {
         // find words that not been remembered in 7 days
-        return dayjs().diff(dayjs(word.lastRememberedDate??Date.now()), 'day') < 7
+        if (!word.lastRememberedDate) return true
+
+        return dayjs().diff(dayjs(word.lastRememberedDate), 'day') > 7
     }).toReversed()
 
     return ws.slice(0, 20);
